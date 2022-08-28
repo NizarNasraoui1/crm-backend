a92f0b928cb7e0837ae70e76cc961c4409bc1e71
NizarNasraoui1 <nizar.al.nasraoui@gmail.com>
2022-08-28 22:20:00 +0200

    added spring security (role,user,filters,configuration)

--- src/main/java/com/crm/Crm/CrmApplication.java
+++ src/main/java/com/crm/Crm/CrmApplication.java
@@ -2,10 +2,15 @@ package com.crm.Crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CrmApplication {
-
+	@Bean
+	org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CrmApplication.class, args);
 	}
